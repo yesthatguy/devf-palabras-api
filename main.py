d47a68b98eccbbb779372cdf3186313e64cb8a15
Brian Salomaki <brian@salomaki.com>
2016-12-05 20:20:09 -0600

    random word endpoint

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -5,11 +7,17 @@ app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+WORDS = open("palabras.txt").read().splitlines()
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World! I\'m Brian!'
+
+@app.route('/random')
+def random_word():
+    """Return a random word."""
+    return random.choice(WORDS)
 
 
 @app.errorhandler(404)
